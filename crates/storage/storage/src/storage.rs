f7b266b08311f572ee521376260572fb811a7c69
Mykhailo Slyvka <misha.slyvka@wallit.eu>
2024-08-08 12:50:55 +0300

    specify block number and network, verify subcommand, async block download

--- crates/storage/storage/src/storage.rs
+++ crates/storage/storage/src/storage.rs
@@ -1,27 +1,48 @@
 use async_trait::async_trait;
-use std::error::Error;
 use std::marker::Send;
+use std::{error::Error, pin::Pin};
 use types::{Block, Transaction};
 
 #[async_trait]
 pub trait Storage: Send {
-    async fn add_block(&mut self, block: Block) -> Result<(), Box<dyn Error>>;
+    async fn add_block(&mut self, block: Block) -> Result<(), Pin<Box<dyn Error + Send + Sync>>>;
+    async fn add_block_with_replace(
+        &mut self,
+        block: Block,
+    ) -> Result<(), Pin<Box<dyn Error + Send + Sync>>>;
 
-    async fn get_block_by_number(&self, block_number: i64) -> Result<Block, Box<dyn Error>>;
-    async fn get_block_by_hash(&self, block_hash: String) -> Result<Block, Box<dyn Error>>;
-    async fn get_all_blocks(&self) -> Result<Vec<Block>, Box<dyn Error>>;
-    async fn get_latest_block_number(&self) -> Result<i64, Box<dyn Error>>;
-    async fn update_blocks_to_matured(&mut self, block_number: i64) -> Result<(), Box<dyn Error>>;
+    async fn get_block_by_number(
+        &self,
+        block_number: i64,
+    ) -> Result<Block, Pin<Box<dyn Error + Send + Sync>>>;
+    async fn get_block_by_hash(
+        &self,
+        block_hash: String,
+    ) -> Result<Block, Pin<Box<dyn Error + Send + Sync>>>;
+    async fn get_all_blocks(&self) -> Result<Vec<Block>, Pin<Box<dyn Error + Send + Sync>>>;
+    async fn get_blocks_in_range(
+        &self,
+        start: i64,
+        end: i64,
+    ) -> Result<Vec<Block>, Pin<Box<dyn Error + Send + Sync>>>;
+    async fn get_latest_block_number(&self) -> Result<i64, Pin<Box<dyn Error + Send + Sync>>>;
+    async fn update_blocks_to_matured(
+        &mut self,
+        block_number: i64,
+    ) -> Result<(), Pin<Box<dyn Error + Send + Sync>>>;
 
     async fn add_transactions(
         &mut self,
         transactions: Vec<Transaction>,
-    ) -> Result<(), Box<dyn Error>>;
+    ) -> Result<(), Pin<Box<dyn Error + Send + Sync>>>;
     async fn get_block_transctions(
         &self,
         block_number: i64,
-    ) -> Result<Vec<Transaction>, Box<dyn Error>>;
-    async fn get_transaction_by_hash(&self, hash: String) -> Result<Transaction, Box<dyn Error>>;
+    ) -> Result<Vec<Transaction>, Pin<Box<dyn Error + Send + Sync>>>;
+    async fn get_transaction_by_hash(
+        &self,
+        hash: String,
+    ) -> Result<Transaction, Pin<Box<dyn Error + Send + Sync>>>;
 
-    async fn prepare_db(&mut self) -> Result<(), Box<dyn Error>>;
+    async fn prepare_db(&mut self) -> Result<(), Pin<Box<dyn Error + Send + Sync>>>;
 }
